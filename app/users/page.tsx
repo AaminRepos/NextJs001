37b65ccd1d776272506a76348b60f712f8b9aa0a
Amins <ahmedtawfik.amin@gmail.com>
2023-09-22 17:36:41 +0300

    Caching

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 
 interface user {
   id: number;
@@ -6,19 +6,24 @@ interface user {
 }
 
 const UserPage = async () => {
+  const res = await fetch(
+    "https://jsonplaceholder.typicode.com/users",
+    //{ cache:'no-store'}); or
+    { next: { revalidate: 10 } }
+  ); // will fwtch new every10 secs
 
-  const res = await fetch('https://jsonplaceholder.typicode.com/users');
-
-  const users: user[] = await res.json(); 
+  const users: user[] = await res.json();
 
   return (
     <>
       <h1>Users</h1>
       <ul>
-        {users.map(user => <li key={user.id}>{user.name}</li>)}
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
       </ul>
     </>
-  )
-}
+  );
+};
 
-export default UserPage
+export default UserPage;
