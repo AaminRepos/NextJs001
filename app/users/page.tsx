f85d3f00fd1273caff0c6d22908cf9624b5eade8
Amins <ahmedtawfik.amin@gmail.com>
2023-09-23 23:21:42 +0300

    static and dynamic rendering

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -8,15 +8,18 @@ interface user {
 const UserPage = async () => {
   const res = await fetch(
     "https://jsonplaceholder.typicode.com/users",
-    //{ cache:'no-store'}); or
-    { next: { revalidate: 10 } }
-  ); // will fwtch new every10 secs
+    { cache:'no-store'}
+    //or
+    //{ next: { revalidate: 10 } }
+    // will fwtch new every10 secs 
+  ); 
 
   const users: user[] = await res.json();
 
   return (
     <>
       <h1>Users</h1>
+      <p>{new Date().toLocaleTimeString()}</p>
       <ul>
         {users.map((user) => (
           <li key={user.id}>{user.name}</li>
