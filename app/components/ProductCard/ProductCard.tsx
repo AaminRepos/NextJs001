2bfe057493b0467c189dc603a1fb56c2f5882411
Amins <ahmedtawfik.amin@gmail.com>
2023-09-24 01:22:22 +0300

    Daisy UI themed and tabled

--- app/components/ProductCard/ProductCard.tsx
+++ app/components/ProductCard/ProductCard.tsx
@@ -3,12 +3,7 @@ import React from "react";
 
 const ProductCard = () => {
   return (
-    <div className='
-      bg-sky-400
-        p-5 my-5 
-        text-white 
-        text-lg 
-        hover:bg-sky-900'>
+    <div>
       <AddToCart />
     </div>
   );
