222f2768ce0f0b3d7da70f893a351d60088f9c40
Amins <ahmedtawfik.amin@gmail.com>
2023-09-24 00:58:29 +0300

    Tailwind CSS

--- app/components/ProductCard/ProductCard.tsx
+++ app/components/ProductCard/ProductCard.tsx
@@ -1,10 +1,14 @@
 import AddToCart from "../AddToCart";
 import React from "react";
-import styles from "./ProductCart.module.css";
 
 const ProductCard = () => {
   return (
-    <div className={styles.card}>
+    <div className='
+      bg-sky-400
+        p-5 my-5 
+        text-white 
+        text-lg 
+        hover:bg-sky-900'>
       <AddToCart />
     </div>
   );
